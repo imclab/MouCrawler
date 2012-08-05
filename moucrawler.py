ceb7dcec1a9f2d6179649a797fc7ed773ac020b1
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-08-04 17:22:14 -1100

    Update moucrawler.py

--- moucrawler.py
+++ moucrawler.py
@@ -51,34 +51,35 @@ class MouCrawler:
 	def crawl(self, link):
 		'''Main crawler recursive function'''
 		for url in self.get_links(link):
-			self.crawl(url)
+			try:
+				self.crawl(url)
+			except IOError:
+				pass
 	
 	def get_links(self, link):
 		'''This function load a page and return all external links into it
-		its also add links to the list of links accessible with self.all_links()'''
+		its also add links to the list of links accessible with self.all_links()
+		raise an IOError if any errors'''
 		if (link in self.tested):
-			return []
+			raise IOError("link already found")
+		
 		self.tested.add(link)
-		try:
-			page = urlopen(link).read()
-		except IOError:
-			return []
+		page = urlopen(link).read()
 		links = set()
+		new = ''
 		domain = ("http://%s" % link.split("/")[2])
 		page_items = (page.split('"') + page.split("'"))
+		
 		for potential_link in page_items:
-			if (potential_link.startswith("http")):
-				new = potential_link
-			elif (potential_link.startswith("/")):
-				new = domain + potential_link
-			else:
-				new = ''
+			if (potential_link.startswith("http")):new = potential_link
+			elif (potential_link.startswith("/")):new = domain + potential_link
 			#begin cut and repair non html links
 			if ("/>" in new):new = new[:new.find("/>")+1]
 			if ("/*" in new):new = new[:new.find("/*")]
 			if (new.count("//") >= 2):new = 'http://' + new.split("//")[2]
 			#end cut and repair non html links
 			links.add(new)
+		
 		self.links = self.links.union(links)
 		if (self.display):
 			stdout.write("\rFound %i urls %i requests done." % (len(self), len(self.tested)))
