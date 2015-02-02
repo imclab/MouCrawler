30250d004df0c7f19b6c0600e1b572d8e60c4cb6
Arnaud Aliès <arnaudalies.py@gmail.com>
2015-02-02 07:14:08 +0100

    Update moucrawler.py

--- moucrawler.py
+++ moucrawler.py
@@ -1,92 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#  moucrawler.py
-#  
-#  Copyright 2012 Arnaud Alies <arnaudalies.py@gmail.com>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-
-__author__ = "Arnaud Alies"
-__version__ = 5.1
-__doc__ = """
-MouCrawler
-
-A python web crawler based on
-the principe if http or / is after a quote
-should be a link
-"""
-
+import urllib
 import httplib
-import random
-from urllib import urlopen, urlretrieve
-from sys import stdout
-from os import fsync, rename, remove, mkdir
+import sqlite3
 
-class crawler:
-	def __init__(self,Display=True):
-		self.tested = set()
-		self.links = set()
-		self.display = Display
-		self.file_name = "links_"+str(random.randint(1,1000))+".txt"
-	
-	def __len__(self):
-		return len(self.links)
-	
-	
-	def all_links(self):
-		"""return all links found"""
-		return list(self.links)
-	
-	def crawl(self, link):
-		'''Main crawler recursive function'''
-		if (link not in self.tested):
-			self.tested.add(link)
-			try:
-				new_links = self.get_links(link)
-			except (IOError, RuntimeError):
-				pass
-		
-		f = open(self.file_name,"a")
-		
-		for url in set(new_links)-set(self.links):
-			f.write("\n"+url)
-		f.close()
-		
-		self.links = self.links.union(new_links)
-		
-		if (self.display):
-			stdout.write("\rFound %i urls %i requests done." % (len(self), len(self.tested)))
-			stdout.flush()
+def get_content(info):
+	content = info.getheaders("Content-Type")
+	if (content):
+		return content[0].split("/")[0]
+	return ""
+
+
+def get_links(link):
+		'''This function load a page and return a set with
+		all external links into it'''
 		
 		try:
-			self.crawl(list(self.links-self.tested)[0])
-		except:
-			print("\nRAM full...")
-
-	
-	def get_links(self, link):
-		'''This function load a page and return all external links into it
-		its also add links to the list of links accessible with self.all_links()
-		raise an IOError if any errors'''
-		
-		try:
-			page = urlopen(link).read()
+			url = urllib.urlopen(link)
+			page = url.read()
 		except (IOError, httplib.InvalidURL, httplib.LineTooLong, TypeError):
-			return []
+			return set()
+		
 		links = set()
 		new = ''
 		domain = ("http://%s" % link.split("/")[2])
@@ -104,36 +36,58 @@ class crawler:
 		
 		return links
 
-def save_links(links):
-	print "\n"+links
 
-def seekAndDownload(links, formats):
-	'''Download all files from x formats
-	example: seekAndDownload(list(crawler.all_links()), ["PNG", "MNG", "TIFF", "JPEG", "GIF", "TGA", "JPG", "RAW"])'''
-	if not formats:
+class Crawler:
+	def __init__(self, start_link):
+		self.id = 1.0
+		self.database = sqlite3.connect("Crawler database.db")
+		self.cursor = self.database.cursor()
+		
+		try:
+			self.cursor.execute("CREATE TABLE links (link text, type text, crawled real, id real)")
+			self.cursor.execute("INSERT INTO links VALUES ('%s', 'text', 0, 1)" % start_link)
+		except (sqlite3.OperationalError):
+			self.id = self.cursor.execute('SELECT max(id) FROM links').fetchone()[0]+1
+		self.database.commit()
+		print self.id
+		
+		
+	
+	def crawl(self):
+		row = self.cursor.execute('SELECT * FROM links WHERE crawled=0 AND type="text" ORDER BY id').fetchone()
+		url = row[0]
+		self.id = row[3]
+		self.cursor.execute("UPDATE links SET crawled=1 WHERE id="+str(self.id))
+		
+		
+		print("Crawling "+url)
+		links = []
+		try:
+			links = list(get_links(url))
+		except:
+			print("Error")
+		self.database.commit()
+
+		
+		for link in links:
+			try:
+				print(link)
+				exist = self.cursor.execute("SELECT * FROM links WHERE link='%s'" % link).fetchall()
+				if len(exist):
+					pass
+				else:
+					content = get_content(urllib.urlopen(link).info())
+					self.id+=self.cursor.execute('SELECT max(id) FROM links').fetchone()[0]+1
+					self.cursor.execute("INSERT INTO links VALUES ('%s', '%s', %d, %d)" % (link,content,0,self.id))
+				
+			except:
+				pass
+		
+		
+		self.database.commit()
+		self.crawl()
 		return 0
-	if ("*" in formats):
-		path = "Crawler Downloads"
-	else:
-		path = "_".join(formats)
-	try:
-		mkdir(path)
-	except:
-		pass
-	for link in links:
-		for image_format in formats:
-				if link.upper().endswith(".%s" % image_format.upper()) or "*" in formats:
-					if ("/" in link):
-						file_name = link.split("/")[len(link.split("/"))-1]
-					else:
-						file_name = link
-					try:
-						print("Downloading: %s" % link)
-						urlretrieve(link, "%s//%s" % (path, file_name))
-					except IOError:
-						pass
-	return 0
+
 
 if (__name__ == "__main__"):
-	c = crawler(True)
-	print(c.crawl("http://www.google.com"))
+	Crawler("http://www.google.com").crawl()
